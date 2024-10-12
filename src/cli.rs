5d8c37b188049d1586051facf4e5b03a7553dd50
Artem <48246993+quertc@users.noreply.github.com>
2024-10-12 20:36:07 +0300

    docs: add StartOpt comments

--- src/cli.rs
+++ src/cli.rs
@@ -13,18 +13,23 @@ pub enum Command {
 
 #[derive(StructOpt)]
 pub struct StartOpt {
+    /// Path to the Berachain Docker node
     #[structopt(long, env = "NODE_PATH")]
     pub path: String,
 
+    /// Cron job schedule for taking snapshots
     #[structopt(long, env = "CRON_JOB_TIME")]
     pub job_time: String,
 
+    /// Enable Google Cloud Storage upload
     #[structopt(short, long)]
     pub gcs: bool,
 
+    /// GCS bucket name (required if `--gcs` is set)
     #[structopt(long, env = "GCS_BUCKET", required_if("gcs", "true"))]
     pub gcs_bucket: Option<String>,
 
+    /// GCS folder path (required if `--gcs` is set)
     #[structopt(long, env = "GCS_FOLDER", required_if("gcs", "true"))]
     pub gcs_folder: Option<String>,
 }
